2a43ae40f5d9315b3c3786f0653a8887fb9fc153
Cameron McCormack <cam@mcc.id.au>
2018-07-27 18:15:19 +1000

    App skeleton that takes a minidump filename and reads it.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+#[macro_use]
+extern crate clap;
+extern crate minidump;
+
+use clap::{App, Arg};
+use minidump::Minidump;
+use std::fmt::{self, Display, Formatter};
+
+#[derive(Debug)]
+enum Error {
+    MinidumpReadFailure(minidump::Error),
+}
+
+impl From<minidump::Error> for Error {
+    fn from(e: minidump::Error) -> Error {
+        Error::MinidumpReadFailure(e)
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match self {
+            Error::MinidumpReadFailure(ref e) => {
+                write!(f, "minidump read failure: {:?}", e)
+            }
+        }
+    }
+}
+
+fn run(minidump_filename: &str) -> Result<(), Error> {
+    let dump = Minidump::read_path(minidump_filename)?;
+
+    Result::Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new(crate_name!())
+        .version(crate_version!())
+        .about(crate_description!())
+        .arg(
+            Arg::with_name("MINIDUMP")
+                .help("Specifies the input minidump file")
+                .index(1)
+                .required(true)
+        )
+        .get_matches();
+
+    let minidump_filename = matches.value_of("MINIDUMP").unwrap();
+
+    if let Err(e) = run(minidump_filename) {
+        eprintln!("{}", e);
+    }
 }

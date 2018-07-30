866574f7235beeddd7c5b136a6f462a1a91b634f
Cameron McCormack <cam@mcc.id.au>
2018-07-30 11:27:56 +1000

    Print errors found.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
 #[macro_use]
 extern crate clap;
 extern crate minidump;
+extern crate pe;
 
 mod symbol_cache;
 
 use clap::{App, Arg};
 use minidump::{Minidump, MinidumpMemory, MinidumpMemoryList, MinidumpModule};
 use minidump::{MinidumpModuleList, Module};
+use pe::{Pe, RVA};
 use std::fmt::{self, Display, Formatter};
+use std::mem;
 use symbol_cache::SymbolCache;
 
 #[derive(Debug)]
 enum Error {
     MinidumpReadFailure(minidump::Error),
     SymbolCacheFailure(symbol_cache::Error),
+    PeFailure(pe::Error),
 }
 
 impl From<minidump::Error> for Error {
@@ -28,6 +32,12 @@ impl From<symbol_cache::Error> for Error {
     }
 }
 
+impl From<pe::Error> for Error {
+    fn from(e: pe::Error) -> Error {
+        Error::PeFailure(e)
+    }
+}
+
 impl Display for Error {
     fn fmt(&self, f: &mut Formatter) -> fmt::Result {
         match self {
@@ -37,6 +47,9 @@ impl Display for Error {
             Error::SymbolCacheFailure(ref e) => {
                 write!(f, "symbol cache failure: {}", e)
             }
+            Error::PeFailure(ref e) => {
+                write!(f, "PE failure: {:?}", e)
+            }
         }
     }
 }
@@ -44,12 +57,12 @@ impl Display for Error {
 #[derive(Clone, Copy)]
 struct Span {
     offset: u64,
-    len: u64,
+    len: u32,
 }
 
 impl Span {
     fn end(&self) -> u64 {
-        self.offset + self.len
+        self.offset + self.len as u64
     }
 
     fn intersect(&self, other: Span) -> Option<Span> {
@@ -60,10 +73,15 @@ impl Span {
         } else if self.end() >= other.end() {
             Some(other)
         } else {
-            Some(Span {
-                offset: other.offset,
-                len: self.end() - other.offset,
-            })
+            let len = self.end() - other.offset;
+            if len > 0xffffffff {
+                None
+            } else {
+                Some(Span {
+                    offset: other.offset,
+                    len: len as u32,
+                })
+            }
         }
     }
 }
@@ -74,18 +92,20 @@ trait MemorySpan {
 
 impl MemorySpan for MinidumpMemory {
     fn span(&self) -> Span {
+        assert!(self.size <= 0xffffffff);
         Span {
             offset: self.base_address,
-            len: self.size,
+            len: self.size as u32,
         }
     }
 }
 
 impl MemorySpan for MinidumpModule {
     fn span(&self) -> Span {
+        assert!(self.size() <= 0xffffffff);
         Span {
             offset: self.base_address(),
-            len: self.size(),
+            len: self.size() as u32,
         }
     }
 }
@@ -140,6 +160,34 @@ fn find_intersections<'a, 'b>(
     intersections
 }
 
+struct Mismatch<'a, 'b> {
+    actual: &'a [u8],
+    expected: &'b [u8],
+    offset: u64,
+}
+
+impl<'a, 'b> Mismatch<'a, 'b> {
+    fn len(&self) -> u32 {
+        assert!(self.actual.len() == self.expected.len());
+        assert!(self.actual.len() <= 0xffffffff);
+        self.actual.len() as u32
+    }
+
+    fn span(&self) -> Span {
+        Span {
+            offset: self.offset,
+            len: self.len(),
+        }
+    }
+}
+
+fn make_rva(addr: u64) -> RVA<[u8]> {
+    assert!(addr <= 0xffffffff, "RVAs should always fit in 32 bits");
+
+    // pe crate doesn't provide a good way to produce an RVA from scratch.
+    unsafe { mem::transmute(addr as u32) }
+}
+
 fn run(minidump_filename: &str) -> Result<(), Error> {
     // Read the minidump file.
     let mut dump = Minidump::read_path(minidump_filename)?;
@@ -156,16 +204,33 @@ fn run(minidump_filename: &str) -> Result<(), Error> {
     modules.sort_by(|a, b| a.base_address().cmp(&b.base_address()));
 
     let intersections = find_intersections(&memories, &modules);
-
     if intersections.is_empty() {
         println!("no memory ranges to check");
         return Ok(());
     }
 
+    let mut found_errors = false;
+
     let urls = vec!["https://symbols.mozilla.org/"];
     let sym_cache = SymbolCache::new(Some("/tmp/symcache"), &urls);
 
+    // Go over each intersecting memory range and compare bytes from the
+    // minidump with bytes from the original image.
     for intersection in intersections {
+
+        let offset = intersection.span.offset;
+        let len = intersection.span.len;
+        let memory_base = intersection.memory.base_address;
+        let module_base = intersection.module.base_address();
+
+        // Get the bytes from the minidump.
+        let (_, memory_bytes) = intersection
+            .memory
+            .bytes
+            .as_slice()
+            .split_at((offset - memory_base) as usize);
+
+        // Load the binary from the symbol server.
         let file = intersection.module.code_file();
         let file = file.split("\\").last().unwrap();
         let code_file = sym_cache.load_code_file(
@@ -180,6 +245,77 @@ fn run(minidump_filename: &str) -> Result<(), Error> {
                 continue;
             }
         };
+
+        // Get the bytes from the binary.
+        let pe = Pe::new(&code_file.data)?;
+        let rva = make_rva(offset - module_base);
+        let code_file_bytes = pe.ref_slice_at(rva, len).unwrap();
+
+        // Compare the bytes to find spans of mismatches.
+        let mut errors: Vec<Mismatch> = vec![];
+        for (i, (a, b)) in memory_bytes
+            .iter()
+            .zip(code_file_bytes.iter())
+            .enumerate()
+        {
+            if a != b {
+                let offset = offset + i as u64;
+                if errors
+                    .last()
+                    .map(|e| e.span().end() == offset)
+                    .unwrap_or(false)
+                {
+                    let error = errors.last_mut().unwrap();
+                    let len = error.len() as usize;
+                    error.actual = &memory_bytes[i - len .. i + 1];
+                    error.expected = &code_file_bytes[i - len .. i + 1];
+                } else {
+                    errors.push(Mismatch {
+                        actual: &memory_bytes[i .. i + 1],
+                        expected: &code_file_bytes[i .. i + 1],
+                        offset,
+                    });
+                }
+            }
+        }
+
+        if errors.is_empty() {
+            continue;
+        }
+        found_errors = true;
+
+        // Print out the errors.
+        let error_count = errors.iter().map(|e| e.len()).fold(0, |sum, i| sum + i);
+
+        println!(
+            "{} mismatching byte{} found in {}",
+            error_count,
+            if error_count == 1 { "" } else { "s" },
+            file
+        );
+
+        for error in errors {
+            println!(
+                "  0x{:08x} .. 0x{:08x} ({} byte{})",
+                error.offset,
+                error.span().end(),
+                error.len(),
+                if error.len() == 1 { "" } else { "s" },
+            );
+            print!("    [");
+            for b in error.actual {
+                print!(" {:02x}", b);
+            }
+            print!(" ] should be [");
+            for b in error.expected {
+                print!(" {:02x}", b);
+            }
+            println!(" ]");
+        }
+    }
+
+    if !found_errors {
+        println!("no errors found");
     }
 
     Ok(())
